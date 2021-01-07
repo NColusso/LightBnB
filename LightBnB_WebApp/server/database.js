7d8b94474bb84e30fea9cf2d9a0ae4ba4f1cb70a
Natasha Colusso <natashacolusso@gmail.com>
2021-01-07 21:44:42 +0000

    feature: getAllReservations now using SQL query to show reservations for signed in user

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -75,7 +75,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `SELECT reservations.*, properties.*, avg(rating)
+  FROM properties
+  JOIN reservations ON properties.id = reservations.property_id
+  JOIN property_reviews ON reservations.property_id = property_reviews.property_id
+  WHERE reservations.guest_id = $1 AND end_date < now()::date
+  GROUP BY reservations.id, properties.id
+  ORDER BY start_date
+  LIMIT $2;`,
+      [guest_id, limit]
+    )
+    .then((res) => res.rows)
+    .catch((err) => err);
 };
 exports.getAllReservations = getAllReservations;
 
